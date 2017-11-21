ff3350c15d5a10f74aa49f195664cd5a30ad434e
Marcelo Adamatti <adamatti@gmail.com>
2017-11-20 23:47:36 -0200

    Moved to /lojinha

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 angular.module('app', ['ngResource','ngRoute','ngSanitize'])
 .config(['$routeProvider',function($routeProvider) {
     $routeProvider
-        .when('/home',  { templateUrl: 'templates/home.html', controller: 'homeCtrl'})
-        .when('/sobre',  { templateUrl: 'templates/sobre.html', controller: 'emptyCtrl'})
+        .when('/home',  { templateUrl: '/lojinha/templates/home.html', controller: 'homeCtrl'})
+        .when('/sobre',  { templateUrl: '/lojinha/templates/sobre.html', controller: 'emptyCtrl'})
         .otherwise({redirectTo: '/home'});
 }])
 .factory('Items', ['$resource',function($resource){
-    return $resource("/json/items.json");
+    return $resource("/lojinha/json/items.json");
 }])
 .controller('homeCtrl', function($scope,Items,$routeParams){
     $scope.search = {vendido:false};
